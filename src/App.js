a1836ad027c4ed74a602552ce6d0b3e31e75f2cd
Emran Shafaq <emran.shafaq@gmail.com>
2020-05-29 12:25:33 -0500

    Comment more things out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, {useState} from 'react';
-import {HashRouter, Route, Link} from 'react-router-dom'
+import React from 'react';
+import {HashRouter, Route} from 'react-router-dom'
 import Sidebar from "./Sidebar.js"
-import { createBrowserHistory } from "history";
+// import { createBrowserHistory } from "history";
 import Preface from "./Preface.js"
 import About from "./About.js"
 import Prologue from "./Prologue.js"
-import NavButton from "./NavButton.js"
+// import NavButton from "./NavButton.js"
 
 function App() {
 
-  const paths = ["Preface", "Prologue", "About"];
-  const history = window.location.href.split("/").slice(-1)[0];
-  let startIndex = 0;
-  if (history.length > 0) {
-        startIndex = (
-          paths.indexOf(
-            history.charAt(0).toUpperCase + history.slice(1)
-          )
-        );
-      }
+  // const paths = ["Preface", "Prologue", "About"];
+  // const history = window.location.href.split("/").slice(-1)[0];
+  // let startIndex = 0;
+  // if (history.length > 0) {
+  //       startIndex = (
+  //         paths.indexOf(
+  //           history.charAt(0).toUpperCase + history.slice(1)
+  //         )
+  //       );
+  //     }
   
-      const [index, setIndex] = useState(startIndex);
+      // const [index, setIndex] = useState(startIndex);
 
 
   return (
