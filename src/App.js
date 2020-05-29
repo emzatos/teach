ef489ba80a8af34c4613badc206f4ca82065eb23
Emran Shafaq <emran.shafaq@gmail.com>
2020-05-29 16:04:04 -0500

    Added import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {HashRouter, Route} from 'react-router-dom'
-//import Sidebar from "./Sidebar.js"
+import Sidebar from "./Sidebar.js"
 // import { createBrowserHistory } from "history";
 import Preface from "./Preface.js"
 import About from "./About.js"
