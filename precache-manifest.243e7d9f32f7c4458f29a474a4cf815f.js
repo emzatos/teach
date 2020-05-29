77de6c81993827a51a1c568783835c50c2f021fc
emzatos <emzatos@users.noreply.github.com>
2020-05-29 21:06:01 +0000

    deploy: ef489ba80a8af34c4613badc206f4ca82065eb23

---
+++ precache-manifest.243e7d9f32f7c4458f29a474a4cf815f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bc0c6fa8590be5c272d0d8890fd8b0",
+    "url": "/teach/index.html"
+  },
+  {
+    "revision": "cfb61ef0444b3fc80a1f",
+    "url": "/teach/static/css/main.f101f74a.chunk.css"
+  },
+  {
+    "revision": "66d21e63a57cd08268f2",
+    "url": "/teach/static/js/2.ccedd8eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teach/static/js/2.ccedd8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb61ef0444b3fc80a1f",
+    "url": "/teach/static/js/main.f9200e65.chunk.js"
+  },
+  {
+    "revision": "15323cdf3c2c025b7d38",
+    "url": "/teach/static/js/runtime-main.9ef99463.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teach/precache-manifest.99e6e5514868515c9518a2e6c2bcad8f.js"
+  "/teach/precache-manifest.243e7d9f32f7c4458f29a474a4cf815f.js"
 );
 
 self.addEventListener('message', (event) => {
